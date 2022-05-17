5efe6a87158ea617226caeb8c5c429b7aa4d4c94
awxkee <qnljoy@gmail.com>
2022-05-17 17:47:36 +0200

    initial release

--- Package.swift
+++ Package.swift
@@ -5,16 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "pdfwriter",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v12)
+    ],
     products: [
         .library(
             name: "pdfwriter",
-            targets: ["libPDFWriter", "libLibTiff", "libLibAesgm", "libFreeType"]),
+            targets: ["pdfwriter"]),
     ],
     dependencies: [
         .package(url: "https://github.com/awxkee/mozjpeg.swift.git", branch: "master"),
         .package(url: "https://github.com/awxkee/libpng.swift.git", branch: "master")
     ],
     targets: [
+        .target(name: "pdfwriter", dependencies: [
+            .target(name: "libPDFWriter"),
+            .target(name: "libLibTiff"),
+            .target(name: "libLibAesgm"),
+            .target(name: "libFreeType"),
+            .product(name: "mozjpeg", package: "mozjpeg.swift"),
+            .product(name: "libpng", package: "libpng.swift")
+        ]),
         .binaryTarget(name: "libPDFWriter", path: "Sources/libPDFWriter.xcframework"),
         .binaryTarget(name: "libLibTiff", path: "Sources/libLibTiff.xcframework"),
         .binaryTarget(name: "libLibAesgm", path: "Sources/libLibAesgm.xcframework"),
---
+++ Sources/pdfwriter/PdfWriter.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Radzivon Bartoshyk on 17/05/2022.
+//
+
+import Foundation
+
+let pdfWriterVersion = "1.0.0"
